0ce90cdeda04426620dec5305ae18d688aa10a47
peeyush0205 <b21112@students.iitmandi.ac.in>
2023-03-03 19:20:54 +0530

    done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/profile";
@@ -23,18 +23,29 @@ function App() {
     //   </header>
     // </div>
     
-      <div className="App">
-          <header className="App-header">
-              <h1>React and flask</h1>
-              {/* Calling a data from setdata for showing */}
-              <p>"hi"</p>
-
-          </header>
-      </div>
+    <>
+    {/* This is the alias of BrowserRouter i.e. Router */}
+    <Router>
+      <Routes>
+      {/* This route is for home component 
+          with exact path "/", in component props 
+          we passes the imported component*/}
+          <Route exact path="/" element={<Home/>} />
+            
+            {/* This route is for about component 
+            with exact path "/aboxut", in component 
+            props we passes the imported component*/}
+            <Route path="/dashboard" element={<About/>} />
+          
+            {/* If any route mismatches the upper 
+          route endpoints then, redirect triggers 
+          and redirects app to home component with to="/" */}
+          <Route to="/" />
+      </Routes>
+    </Router>
+  </>
 
   );
 }
 
 export default App;
-
-
