5a0f04983412d12d7a87cadf8c9529796c913906
Aishal Gupta <aishalgupta3008@gmail.com>
2023-03-03 18:49:22 +0530

    landing pg

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Main from './main';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    
-      <div className="App">
-          <header className="App-header">
-              <h1>React and flask</h1>
-              {/* Calling a data from setdata for showing */}
-              <p>"hi"</p>
-
-          </header>
-      </div>
-
+   <Main/>
   );
 }
 
 export default App;
+
+
