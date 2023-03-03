b40827392a90d01f9c0deb78ed76fe5744e47fa1
peeyush0205 <b21112@students.iitmandi.ac.in>
2023-03-03 19:15:28 +0530

    done

---
+++ my-app/src/Pages/dashboard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+  } from "react-router-dom";
+const About = () => {
+return (
+	<div>
+	<h1>About Page</h1>
+	</div>
+);
+};
+
+export default About;
