941972dcfe3a9edca0b3d734cfaccc5ef27f7019
ShreyaGarg01 <shreyagarg0112@gmail.com>
2023-03-04 11:39:39 +0530

    Changes

--- app.py
+++ app.py
@@ -89,84 +89,11 @@ def login():
                 if "email" in session:
                     return redirect(url_for("logged_in"))
                 message = 'Wrong password'
-                return render_template('login_shopkeeper.html', message=message)
+                return render_template('login.html', message=message)
         else:
             message = 'Email not found'
-            return render_template('login_shopkeeper.html', message=message)
-    return render_template('login_shopkeeper.html', message=message)
-
-
-#  customers
-@app.route("/customer_signup", methods=['post', 'get'])
-def index1():
-    message = ''
-    #if method post in index
-    if "email" in session:
-        return redirect(url_for("logged_in"))
-    if request.method == "POST":
-        user = request.form.get("fullname")
-        email = request.form.get("email")
-       
-        
-        username = request.form.get("username")
-        
-        password1 = request.form.get("password1")
-        password2 = request.form.get("password2")
-        #if found in database showcase that it's found 
-        user_found = records.find_one({"name": user})
-        email_found = records.find_one({"email": email})
-        if user_found:
-            message = 'There already is a user by that name'
-            return render_template('customer.html', message=message)
-        if email_found:
-            message = 'This email already exists in database'
-            return render_template('customer.html', message=message)
-        if password1 != password2:
-            message = 'Passwords should match!'
-            return render_template('customer.html', message=message)
-        else:
-            #hash the password and encode it
-            hashed = bcrypt.hashpw(password2.encode('utf-8'), bcrypt.gensalt())
-            #assing them in a dictionary in key value pairs
-            user_input = {'name': user, 'email': email, 'password': hashed, 'type': type,  'username': username, }
-            #insert it in the record collection
-            records.insert_one(user_input)
-            
-            #find the new created account and its email
-            user_data = records.find_one({"email": email})
-            new_email = user_data['email']
-            #if registered redirect to logged in as the registered user
-            return render_template('logged_in.html', email=new_email)
-    return render_template('customer.html')
-
-@app.route("/customer_login", methods=["POST", "GET"])
-def login1():
-    message = 'Please login to your account'
-    if "email" in session:
-        return redirect(url_for("logged_in"))
-
-    if request.method == "POST":
-        email = request.form.get("email")
-        password = request.form.get("password")
-
-        #check if email exists in database
-        email_found = records.find_one({"email": email})
-        if email_found:
-            email_val = email_found['email']
-            passwordcheck = email_found['password']
-            #encode the password and check if it matches
-            if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
-                session["email"] = email_val
-                return redirect(url_for('logged_in'))
-            else:
-                if "email" in session:
-                    return redirect(url_for("logged_in"))
-                message = 'Wrong password'
-                return render_template('login_customer.html', message=message)
-        else:
-            message = 'Email not found'
-            return render_template('login_customer.html', message=message)
-    return render_template('login_customer.html', message=message)
+            return render_template('login.html', message=message)
+    return render_template('login.html', message=message)
 
 @app.route('/logged_in')
 def logged_in():
@@ -182,7 +109,7 @@ def logout():
         session.pop("email", None)
         return render_template("signout.html")
     else:
-        return render_template('main.html')
+        return render_template('index.html')
 
 # render home page
 @ app.route('/')
